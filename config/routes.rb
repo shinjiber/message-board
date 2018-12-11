260e21e15a13883d24c439b39d1d101f0613e319
jienshinji.yamaguchi <shinjiber7@gmail.com>
2018-12-11 00:04:30 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'messages#index'
+  
+  #CURD
+  get 'messages/:id' => 'messages#show'
+  post 'messages' => 'messages#create'
+  put 'messages/:id' => 'messages#update'
+  delete'messages/:id' => 'messages#destroy'
+  
+  #index: showの補助ページ
+  get 'messages' => 'messages#index'
+  
+  #new: 新規作成用のフォームページ
+  get 'messages/new' => 'messages#new'
+  
+  #edit: 更新用のフォームページ
+  get 'messages/edit' => 'messages#edit'
+  
 end
